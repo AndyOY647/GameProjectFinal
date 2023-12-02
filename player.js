db0e66a44267986d760aa55b1dbeb2e60aa82012
AndyOY647 <60327435+AndyOY647@users.noreply.github.com>
2023-12-02 14:25:13 -0500

    walking animation and able to walk around

--- player.js
+++ player.js
@@ -1,22 +1,64 @@
+
+// Call in preload
 class Player{
-    constructor(){
-        this.x_offset= 128;
-        this.y_offset = 100;
-        this.Player = new Sprite(400,400, 128,100);
-        this.Player.spriteSheet = 'player.png';
-        this.Player.anis.offset.x = 0;
-        this.Player.anis.offset.y = 0;
-        this.Player.anis.frameDelay = 10;
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.w = 128;
+        this.h = 128;
+        this.Player = new Sprite(x,y, this.w,this.h);
         this.Player.debug = true;
+        this.direction = 'right';
+        this.speed = 5;
 
-        this.Player.addAnis({
-            idle: {row: 0, frames: 1},
-            runRight: {row: 1, frames: 1},
-            runLeft: {row: 2, frames: 1}
-        });
-        this.player.changeAni('idle');
+    }// constructor ends
 
+
+    // call in setup
+    loadAnimation(){
+        //load Animation
+        idleAni = loadAnimation('PlayerAnimation/idle_2.png', { frameSize: [this.w, this.h], frames: 3 });
+        runAni = loadAnimation('PlayerAnimation/Run.png', { frameSize: [this.w, this.h], frames: 8 });
+        runLeftAni = loadAnimation('PlayerAnimation/Run_Left.png', { frameSize: [this.w, this.h], frames: 8 });
         
+        //Setting Delays
+        runAni.frameDelay = 4;
+        runLeftAni.frameDelay = 4;
+        idleAni.frameDelay = 10;
         
     }
+    // call in draw
+    playAnimation(ani){
+        if (ani == 'idle'){
+            idle = animation(idleAni, this.x, this.y);
+        }
+        else if(ani == 'run'){
+            run = animation(runAni, this.x, this.y);
+        }
+        else if(ani == 'runLeft'){
+            runLeft = animation(runLeftAni, this.x, this.y);
+        }
+    }
+    
+    movement(){
+        if(kb.pressing('right')){
+            this.playAnimation('run');
+            this.direction = 'right';
+            this.Player.mirror.x = false;
+            this.x += this.speed;
+        }
+        else if(kb.pressing('left')){
+            this.playAnimation('runLeft');
+            this.direction = 'left';
+            this.speed;
+            this.Player.mirror.x = true;
+            this.x -= this.speed;
+        }
+        else{
+            this.playAnimation('idle');
+
+
+        }
 }
+
+}// class Player ends
--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
+let idleAni;
+let idle;
+let runAni;
+let run;
+let runLeftAni;
+let runLeft;
 
 function preload() {
-  player = new Player();
+  player = new Player(200,200);
 }
 
 
 function setup() {
   createCanvas(800, 800);
- 
+  player.loadAnimation();
   
 }
 
 function draw() {
   background(0);
+  player.movement();
   
 }
